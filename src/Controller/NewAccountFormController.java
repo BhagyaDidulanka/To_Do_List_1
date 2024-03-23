1d8e750c1fe53b1f684992083f0c2b38a588e27b
BhagyaDidulanka <didulankabhagya@gmail.com>
2024-03-23 09:45:50 +0530

    Add three inteface

---
+++ src/Controller/NewAccountFormController.java
@@ -0,0 +1,10 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+
+public class NewAccountFormController {
+
+    public void btnRegisterOnAction(ActionEvent actionEvent) {
+
+    }
+}
---
+++ src/Controller/ToDoListFormController.java
@@ -0,0 +1,40 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+public class ToDoListFormController<text> {
+
+    public AnchorPane root;
+    public Label msgWelcome;
+    public Pane subRoot;
+    public TextField txtDescription;
+    public Label srId;
+
+    public void initialize(){
+
+    }
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnAddToDoListOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void btnAddToListOnAction(ActionEvent actionEvent) {
+
+
+    }
+
+    public void btnLogOutOnAction(ActionEvent actionEvent) {
+
+    }
+}
