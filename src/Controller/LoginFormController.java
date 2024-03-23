b22ffcc437ecbb16221dc1f3c5817b1bde5e10ec
BhagyaDidulanka <didulankabhagya@gmail.com>
2024-03-23 10:45:33 +0530

    Navigate to Create New Account

--- src/Controller/LoginFormController.java
+++ src/Controller/LoginFormController.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -13,8 +14,9 @@ import java.io.IOException;
 
 public class LoginFormController {
 
-    public AnchorPane root;
+    //public AnchorPane root;
     public static TextField usrname;
+    public AnchorPane root;
 
 
     public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
@@ -24,8 +26,20 @@ public class LoginFormController {
 
         Stage stage=(Stage) root.getScene().getWindow();
         stage.setScene(scene);
+        stage.setTitle("To Do List");
         stage.centerOnScreen();
         stage.show();
 
+    }
+
+    public void lblCreateNewAccountOnMouseClick(MouseEvent mouseEvent) throws IOException {
+        Parent parent=FXMLLoader.load(this.getClass().getResource("../view/NewAccountForm.fxml"));
+        Scene scene=new Scene(parent);
+
+        Stage stage=(Stage) root.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Create New Account");
+        stage.centerOnScreen();
+        stage.show();
     }
 }
--- src/Controller/NewAccountFormController.java
+++ src/Controller/NewAccountFormController.java
@@ -1,8 +1,11 @@
 package Controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
 
 public class NewAccountFormController {
+
+    public AnchorPane root;
 
     public void btnRegisterOnAction(ActionEvent actionEvent) {
 
