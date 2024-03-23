f1938cd3bb59192a5eee9755d9389476bfa8101e
BhagyaDidulanka <didulankabhagya@gmail.com>
2024-03-23 21:51:59 +0530

    add mysql-conecter-libry and load classes from that library

--- src/db/DBconection.java
+++ src/db/DBconection.java
@@ -4,7 +4,11 @@ public class DBConection {
     private static DBConection dbConection;
 
     private DBConection(){
-
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     public static DBConection getInstance(){
