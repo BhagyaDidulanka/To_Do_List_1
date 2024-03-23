9e0336ec5a3e09bc314343ca3d128bcfed571dff
BhagyaDidulanka <didulankabhagya@gmail.com>
2024-03-23 22:02:40 +0530

    apply database connection

--- src/db/DBconection.java
+++ src/db/DBconection.java
@@ -1,12 +1,20 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConection {
     private static DBConection dbConection;
+    private Connection connection;
 
     private DBConection(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+
+            DriverManager.getConnection("jdbc:mysql://localhost:3306/todolist","root","");
+
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
@@ -16,5 +24,9 @@ public class DBConection {
              dbConection=new DBConection();
         }
         return dbConection;
+    }
+
+    public Connection getConnection(){
+        return connection;
     }
 }
